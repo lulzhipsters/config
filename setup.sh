b84de28604528ba1e6b09604452fb14d4514ef08
lulzhipsters <lewis.j.hopkins@gmail.com>
2024-11-29 16:58:32 +1000

    Avoid using alias in sh script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 REMOTE='https://github.com/lulzhipsters/config.git'
 
+git init $HOME
+echo '*' >> $HOME/.git/info/exclude
+
 alias config='/usr/bin/git --git-dir=$HOME/.git --work-tree=$HOME'
 
-config init
-echo '*' >> $HOME/.git/info/exclude
-config remote add "origin" $REMOTE
-config checkout main
+git -C $HOME remote add "origin" $REMOTE
+git -C $HOME checkout main

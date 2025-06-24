52ced4400aef74cefba2b11272245683f45bb6c8
lulzhipsters <lewis.j.hopkins@gmail.com>
2025-06-24 11:27:52 +1000

    tweak vscode, ps profile and omp settings

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 oh-my-posh init pwsh --config '~/.oh-my-posh/pretty-in-pink.json' | Invoke-Expression
 
-Set-Alias -Name ll -Value gci -Force
+function Get-ChildItemForce { Get-ChildItem -Force @args }
+Set-Alias -Name ll -Value Get-ChildItemForce
 
 Set-Alias -Name lg -Value lazygit

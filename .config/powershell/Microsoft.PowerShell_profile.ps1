3fc0f825802e248c21a343081ac075a9be95f6bb
lulzhipsters <lewis.j.hopkins@gmail.com>
2026-03-30 11:18:50 +1000

    omp -> starship, vscode updates, opencode model config

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,8 @@
-oh-my-posh init pwsh --config '~/.oh-my-posh/pretty-in-pink.json' | Invoke-Expression
+#oh-my-posh init pwsh --config '~/.oh-my-posh/pretty-in-pink.json' | Invoke-Expression
 
 function Get-ChildItemForce { Get-ChildItem -Force @args }
 Set-Alias -Name ll -Value Get-ChildItemForce
 
 Set-Alias -Name lg -Value lazygit
+
+Invoke-Expression (&starship init powershell)
